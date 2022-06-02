7c112f3f23f3e2bf4e01eefc3edcdb8bfa80357a
Ondřej Filip <zoomblick@gmail.com>
2022-06-02 11:50:48 +0200

    Init MariaDB Connection

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 
+from database import SessionLocal
+
 app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
