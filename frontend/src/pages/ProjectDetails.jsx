3985e239ff57868a4d6e38d244d1bd0253d51a3c
Ondřej Filip <zoomblick@gmail.com>
2022-11-14 15:50:32 +0100

    Fix Project details buttons

--- frontend/src/pages/ProjectDetails.jsx
+++ frontend/src/pages/ProjectDetails.jsx
@@ -67,36 +67,42 @@ const ProjectDetailsPage = () => {
             <div className="mt-12 mx-auto max-w-screen-xl text-center">
               <h1 className="text-3xl font-bold md:text-4xl mb-12">{project.title}</h1>
               <p>{project.description}</p>
-              <div className={'mt-12 flex justify-center'}>
-                <button className="btn btn-primary mx-2 gap-2" onClick={handleDownload}>
-                  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" /></svg>
-                  Download Dataset
-                </button>
-                <Link
-                  to={`/projects/${project.id}/columns-view`}
-                  className={'btn btn-primary mx-2 gap-2'}>
-                  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z" /></svg>
-                  View Columns
-                </Link>
-                <Link
-                  to={`/projects/edit/${project.id}`}
-                  className={'btn btn-info mx-2 btn-outline gap-2'}>
-                  <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" /></svg>
-                  Edit Project
-                </Link>
-                <button
-                  onClick={handleDelete}
-                  className={`btn ${
-                    project.isDeleting ? 'loading' : ''
-                  } mx-2 btn-error btn-outline gap-2`}
-                >
-                  {!project.isDeleting && (
-                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>
-                  )}
-                  Delete Project
-                </button>
+              <div className={'mt-12 flex justify-center flex-col md:flex-row items-center'}>
+                <div className="btn-group mx-2 lg:mx-4 my-2">
+                  <button className="btn gap-2 hover:text-info" onClick={handleDownload}>
+                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" /></svg>
+                    Download Dataset
+                  </button>
+                  <Link
+                    to={`/projects/${project.id}/columns-view`}
+                    className={'btn gap-2 hover:text-info'}>
+                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z" /></svg>
+                    View Columns
+                  </Link>
+                </div>
+                <div className="btn-group mx-2 lg:mx-4 my-2">
+                  <Link
+                    to={`/projects/${project.id}/columns-check`}
+                    className={'btn gap-2 hover:text-info'}>
+                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 17V7m0 10a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h2a2 2 0 012 2m0 10a2 2 0 002 2h2a2 2 0 002-2M9 7a2 2 0 012-2h2a2 2 0 012 2m0 10V7m0 10a2 2 0 002 2h2a2 2 0 002-2V7a2 2 0 00-2-2h-2a2 2 0 00-2 2" /></svg>
+                  </Link>
+                  <Link
+                    to={`/projects/edit/${project.id}`}
+                    className={'btn gap-2 hover:text-info'}>
+                    <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" /></svg>
+                  </Link>
+                  <button
+                    onClick={handleDelete}
+                    className={`btn ${
+                      project.isDeleting ? 'loading' : ''
+                    } gap-2 hover:text-error`}
+                  >
+                    {!project.isDeleting && (
+                      <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>
+                    )}
+                  </button>
+                </div>
               </div>
-
               {loadingDatasetView ? (
                 <div className={'my-20'}>
                   <Loading />
